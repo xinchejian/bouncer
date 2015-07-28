cc89518e0ab15f0efbbe22f84a1b089fc043441d
Lionello Lunesu <lio+git@lunesu.com>
2015-07-28 16:41:33 +0800

    Show 'Old' for existing member payments; change email signature

--- submit.php
+++ submit.php
@@ -46,6 +46,7 @@ $salt2 = '"'.mysql_real_escape_string($salt, $link).'"';
 
 mysql_query("INSERT IGNORE members.Users (email,since) VALUES($email2,NOW())", $link)
 	or mail_and_die('mysql_query INSERT Users error');
+$isnew = mysql_affected_rows($link) == 1;
 
 mysql_query("INSERT members.Payments (email, submitted, amount) VALUES($email2, NOW(), $amount2)", $link)
 	or mail_and_die('mysql_query INSERT Payments error');
@@ -71,7 +72,11 @@ Note that your access will be revoked if no payment was made.
 -- the script that sends out these emails";
 mailer($email, $subject, $body);
 
-mailer('staff@xinchejian.com', "New member: $email, paid $amount for $months month(s).", '-- the script that sends out these emails');
+if ($isnew)
+	$neworold = "New";
+else
+	$neworold = "Old";
+mailer('staff@xinchejian.com', "$neworold member: $email, paid $amount for $months month(s).", '-- '.__FILE__);
 
 header('HTTP/1.1 303 See Other');
 header('Location: /welcome.html');
